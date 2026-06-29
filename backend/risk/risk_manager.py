4672867724050a075f0519aed4ad6147a8200a46
cornichepalace <asajee70@gmail.com>
2026-06-29 18:52:40 +0400

    Update risk_manager.py

--- backend/risk/risk_manager.py
+++ backend/risk/risk_manager.py
@@ -1,1 +1,15 @@
+def calculate_risk(balance):
 
+    risk = balance * 0.01
+
+    return round(risk, 2)
+
+
+def stop_loss(entry):
+
+    return round(entry * 0.98, 2)
+
+
+def take_profit(entry):
+
+    return round(entry * 1.04, 2)
