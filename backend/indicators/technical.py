06ab0e69df70984ed8be016f192dfa53e05b3cd6
cornichepalace <asajee70@gmail.com>
2026-06-29 18:33:30 +0400

    Update technical.py

--- backend/indicators/technical.py
+++ backend/indicators/technical.py
@@ -1,8 +1,17 @@
-import pandas as pd
+import ta
 
 def add_indicators(df):
-    # Simple Moving Averages
-    df["sma20"] = df["close"].rolling(20).mean()
-    df["sma50"] = df["close"].rolling(50).mean()
+
+    # RSI
+    df["rsi"] = ta.momentum.RSIIndicator(df["close"]).rsi()
+
+    # EMA
+    df["ema20"] = ta.trend.EMAIndicator(df["close"], window=20).ema_indicator()
+    df["ema50"] = ta.trend.EMAIndicator(df["close"], window=50).ema_indicator()
+
+    # MACD
+    macd = ta.trend.MACD(df["close"])
+    df["macd"] = macd.macd()
+    df["macd_signal"] = macd.macd_signal()
 
     return df
