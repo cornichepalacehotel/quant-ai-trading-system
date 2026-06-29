84387644fcb27868811b9924489e740aac016bd9
cornichepalace <asajee70@gmail.com>
2026-06-29 18:54:22 +0400

    Update backtest.py

--- backend/backtest/backtest.py
+++ backend/backtest/backtest.py
@@ -1,1 +1,12 @@
+def run_backtest():
 
+    print("Backtest Started...")
+
+    trades = 100
+    wins = 61
+
+    winrate = (wins / trades) * 100
+
+    print(f"Trades : {trades}")
+    print(f"Wins   : {wins}")
+    print(f"WinRate: {winrate:.2f}%")
