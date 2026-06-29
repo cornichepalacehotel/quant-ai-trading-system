32b7e0ab2d803498f7230d5ba4f8f186358e3ed0
cornichepalace <asajee70@gmail.com>
2026-06-29 18:30:12 +0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
-print("Quant AI Trading System Starting...")
+from data.binance_data import get_data
+from indicators.technical import add_indicators
+from signals.signal_engine import generate_signal
 
 def main():
-    print("Bot initialized successfully.")
+
+    print("🚀 Quant AI Trading System Started")
+
+    df = get_data()
+
+    df = add_indicators(df)
+
+    signal = generate_signal(df)
+
+    print(f"Latest Price : {df['close'].iloc[-1]}")
+    print(f"Signal       : {signal}")
 
 if __name__ == "__main__":
     main()
