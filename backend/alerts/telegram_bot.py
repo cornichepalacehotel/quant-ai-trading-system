d381349c888359b127645011822dac4e4f0de48d
cornichepalace <asajee70@gmail.com>
2026-06-29 18:53:31 +0400

    Update telegram_bot.py

--- backend/alerts/telegram_bot.py
+++ backend/alerts/telegram_bot.py
@@ -1,1 +1,18 @@
+import requests
+from config import *
 
+def send_message(message):
+
+    if TELEGRAM_BOT_TOKEN == "" or TELEGRAM_CHAT_ID == "":
+        print("Telegram not configured")
+        return
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        data={
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": message
+        }
+    )
