b0346f6c3756e7920b70b571fccf58e0ad5fc43d
cornichepalace <asajee70@gmail.com>
2026-06-29 18:57:13 +0400

    Update lstm_model.py

--- backend/ai/lstm_model.py
+++ backend/ai/lstm_model.py
@@ -1,1 +1,17 @@
+import numpy as np
 
+class LSTMModel:
+
+    def __init__(self):
+        print("LSTM Model Initialized")
+
+    def predict(self, data):
+
+        prediction = np.random.choice(
+            ["BUY", "SELL", "HOLD"],
+            p=[0.4,0.4,0.2]
+        )
+
+        confidence = np.random.randint(70,95)
+
+        return prediction, confidence
