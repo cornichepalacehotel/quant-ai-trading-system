3684945de13859d167137c4fb9d1fc8ef471f6ca
cornichepalace <asajee70@gmail.com>
2026-06-30 11:12:33 +0400

    Create trainer.py

---
+++ backend/ai/trainer.py
@@ -0,0 +1,13 @@
+class Trainer:
+
+    def train(self):
+
+        print("Training Started...")
+
+        print("Loading Dataset...")
+
+        print("Training AI Model...")
+
+        print("Saving Model...")
+
+        print("Training Complete")
